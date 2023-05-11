083978ff6ee146e340e6548b47b2e121c7210547
oleksandr-yakov <alex.yakov1109@gmail.com>
2023-05-12 02:47:30 +0300

    fix  tests

--- App/start_test.sh
+++ App/start_test.sh
@@ -3,9 +3,9 @@
 exists_all=$(docker ps -qf name=once* | wc -l)
 needed_conteiner=$(docker ps -aqf name=app)
 
-if [ ${exists_all} -eq 3  ]; then
+if [ ${exists_all} -eq 2  ]; then
 	echo "all needs conteiner run and exeists"
-	docker exec -it -u0 ${needed_conteiner}  python3 testauto.py
+	docker exec --tty=false -u0 ${needed_conteiner}  python3 testauto.py
 
 else
 	echo "whats going wrong"
