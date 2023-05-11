47abb8569e1a4d34c7f2ff1f396980ce2ff3d0d1
oleksandr-yakov <alex.yakov1109@gmail.com>
2023-05-12 02:05:47 +0300

    fix create table

--- create-table.sh
+++ create-table.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mysql_host=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' once_mysql)
+mysql_host=$(sudo docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' once_mysql)
 
 DB_USER="dev"
 DB_PASS="Ag111^@ergnuio"
 DB_NAME="prodMain"
 
-docker exec -it once_mysql mysql -u $DB_USER -p$DB_PASS -h $mysql_host -D $DB_NAME -e "CREATE TABLE if not exists usersdata(id INT NOT NULL AUTO_INCREMENT,username VARCHAR(50) NOT NULL,password VARCHAR(50) NOT NULL,PRIMARY KEY (id));"
+sudo docker exec -it once_mysql mysql -u $DB_USER -p$DB_PASS -h $mysql_host -D $DB_NAME -e "CREATE TABLE if not exists usersdata(id INT NOT NULL AUTO_INCREMENT,username VARCHAR(50) NOT NULL,password VARCHAR(50) NOT NULL,PRIMARY KEY (id));"
