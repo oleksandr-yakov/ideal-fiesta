c1d3e1b7823810080356fd3f34c3cfd989ca59af
oleksandr-yakov <alex.yakov1109@gmail.com>
2023-05-12 00:06:09 +0300

    add create table usersdata

---
+++ create-table.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mysql_host=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' once_mysql)
+
+DB_USER="dev"
+DB_PASS="Ag111^@ergnuio"
+DB_NAME="prodMain"
+
+mysql -u $DB_USER -p$DB_PASS -h $mysql_host -D $DB_NAME -e "CREATE TABLE if not exists usersdata(id INT NOT NULL AUTO_INCREMENT,username VARCHAR(50) NOT NULL,password VARCHAR(50) NOT NULL,PRIMARY KEY (id));"  
