957996f571b966c7872ed0e2710d764e242d6825
Artem Bordi <bordyanartem@gmail.com>
2023-05-14 21:28:34 +0300

    fds

--- App/app.py
+++ App/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import requests, datetime, mysql.connector
 from config import TOKEN
 
+#fdsfsd
+
 app = Flask(__name__)
 
 db = mysql.connector.connect(
