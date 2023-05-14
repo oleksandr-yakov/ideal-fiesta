a1eaf4e73d1e40ba481edc0e4f129f086781fced
aleksandr <75263094+oleksandr-yakov@users.noreply.github.com>
2023-05-14 22:26:37 +0300

    Update create-table.sh

--- create-table.sh
+++ create-table.sh
@@ -9,3 +9,4 @@ DB_PASS="Ag111^@ergnuio"
 DB_NAME="prodMain"
 
 sudo docker exec --tty=false once_mysql mysql -u $DB_USER -p$DB_PASS -h $mysql_host -D $DB_NAME -e "CREATE TABLE if not exists usersdata(id INT NOT NULL AUTO_INCREMENT,username VARCHAR(50) NOT NULL,password VARCHAR(50) NOT NULL,PRIMARY KEY (id));"
+echo "finish"
